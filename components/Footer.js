3dea514185fec4db217349b14a2a62f3a9fc2008
Vinicius Zancheta <vinicius.zancheta@unesp.br>
2024-10-18 09:51:29 -0300

    2

--- components/Footer.js
+++ components/Footer.js
@@ -4,13 +4,10 @@ import styles from "../styles/Home.module.css";
 const Footer = () => {
     return (
         <footer className={styles.footer}>
-            <a
-                href="https://domthedev.com"
-                target="_blank"
-                rel="noopener noreferrer"
+            <p
             >
-                Powered by{' '}Dom the dev
-            </a>
+                Todos os Direitos Reservados
+            </p>
         </footer>
     );
 };
